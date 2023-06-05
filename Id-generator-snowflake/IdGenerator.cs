b8c0ea164259edc73a13a6f37962e29f8b93d5c2
Longkunz <cuvip1325@gmail.com>
2023-06-05 22:38:37 +0700

    Setup generator

---
+++ Id-generator-snowflake/IdGenerator.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Id_generator_snowflake
+{
+    public class IdGenerator
+    {
+        public const long Twepoch = 1288834974000L;
+
+        private const int WorkerIdBits = 3;
+
+        private const int DatacenterIdBits = 2;
+
+        private const int SequenceBits = 8;
+
+        private const long MaxWorkerId = -1L ^ (-1L << WorkerIdBits);
+
+        private const long MaxDatacenterId = -1L ^ (-1L << DatacenterIdBits);
+
+        private const long SequenceMask = -1L ^ (-1L << SequenceBits);
+
+        private const int WorkerIdShift = SequenceBits;
+
+        private const int DatacenterIdShift = SequenceBits + WorkerIdBits;
+
+        public const int TimestampLeftShift = SequenceBits + WorkerIdBits + DatacenterIdBits;
+
+        private long _sequence = 0L;
+        private long _lastTimestamp = -1L;
+
+        public long WorkerId { get; protected set; }
+
+        public long DatacenterId { get; protected set; }
+
+        public long Sequence
+        {
+            get { return _sequence; }
+            internal set { _sequence = value; }
+        }
+
+        public IdGenerator(long workerId, long datacenterId, long sequence = 0L)
+        {
+            if (workerId > MaxWorkerId || workerId < 0)
+            {
+                throw new ArgumentException($"worker Id must greater than or equal 0 and less than or equal {MaxWorkerId}");
+            }
+
+            if (datacenterId > MaxDatacenterId || datacenterId < 0)
+            {
+                throw new ArgumentException($"datacenter Id must greater than or equal 0 and less than or equal {MaxDatacenterId}");
+            }
+
+            WorkerId = workerId;
+            DatacenterId = datacenterId;
+            _sequence = sequence;
+        }
+
+        private readonly object _lock = new object();
+
+        public long NextId()
+        {
+            lock (_lock)
+            {
+                var timestamp = TimeGen();
+                if (timestamp < _lastTimestamp)
+                {
+                    throw new Exception($"timestamp error");
+                }
+
+                if (_lastTimestamp == timestamp)
+                {
+                    _sequence = (_sequence + 1) & SequenceMask;
+
+                    if (_sequence == 0)
+                    {
+                        timestamp = TilNextMillis(_lastTimestamp);
+                    }
+                }
+                else
+                {
+                    _sequence = 0;
+                }
+
+                _lastTimestamp = timestamp;
+                return ((timestamp - Twepoch) << TimestampLeftShift) | (DatacenterId << DatacenterIdShift) | (WorkerId << WorkerIdShift) | _sequence;
+            }
+        }
+
+        private long TilNextMillis(long lastTimestamp)
+        {
+            var timestamp = TimeGen();
+            while (timestamp <= lastTimestamp)
+            {
+                timestamp = TimeGen();
+            }
+
+            return timestamp;
+        }
+
+        private long TimeGen()
+        {
+            return DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        }
+    }
+}
